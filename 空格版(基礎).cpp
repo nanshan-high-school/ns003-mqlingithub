cbf107993e269a0e19eba066b6dda8746950fd72
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2019-03-24 13:57:39 +0800

    Create 空格版(基礎).cpp

---
+++ 空格版(基礎).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+using namespace std; 
+
+int main() {
+  stack <int> num;
+  int t;
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    char math;
+    cin >> math;
+    if (math == '+') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() + num1;
+      num.push(ans);
+    }
+
+    else if (math == '-') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() - num1;
+      num.push(ans);
+    }
+
+    else if (math == '*') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() * num1;
+      num.push(ans);
+    }
+
+    else if (math == '/') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() / num1;
+      num.push(ans);
+    }
+
+    else {
+      int a = math - '0';
+      num.push(a);
+    }
+  }
+  cout << num.top();
+}
