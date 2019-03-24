f3a1d5bb64fc20f244f5cdb04dc5ec864a846bae
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2019-03-24 13:33:36 +0800

    Create 一行版.cpp

---
+++ 一行版.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+using namespace std; 
+
+int main() {
+  stack <int> num;
+  string math;
+  cin >> math;
+
+  for (int i = 0; math[i] != '\0'; i++) {
+    if (math[i] == '+') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() + num1;
+      num.push(ans);
+    }
+
+    else if (math[i] == '-') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() - num1;
+      num.push(ans);
+    }
+
+    else if (math[i] == '*') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() * num1;
+      num.push(ans);
+    }
+
+    else if (math[i] == '/') {
+      int num1 = num.top();
+      num.pop();
+      int ans = num.top() / num1;
+      num.push(ans);
+    }
+
+    else {
+      int a = math[i] - '0';
+      num.push(a);
+    }
+  }
+  cout << num.top();
+}
